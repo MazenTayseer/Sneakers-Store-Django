05c9a9f911bf6e28e3b94b63a1651b6ccc0e2fec
MazenTayseer <maziiin360@yahoo.com>
2023-08-22 19:10:22 +0300

    Update admin

--- Ecommerce_store/urls.py
+++ Ecommerce_store/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('CowMoon/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('accounts.urls'))
 ]
 
